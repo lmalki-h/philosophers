b5a897da8d3840731b79a47757e084e7516544f2
lmalki-h <lmalki-h@student.42.fr>
2021-04-13 13:25:49 +0200

    normed

--- philo_one/src/print_status.c
+++ philo_one/src/print_status.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_status.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmalki-h <lmalki-h@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/13 13:14:47 by lmalki-h          #+#    #+#             */
+/*   Updated: 2021/04/13 13:25:18 by lmalki-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/philo_one.h"
 
 static int	ft_strlen(char *s)
@@ -12,33 +24,38 @@ static int	ft_strlen(char *s)
 	return (i);
 }
 
-int		print_status(t_phil *phil, char *action)
+static void	ft_strlcat(char *dst, const char *src, int len_dst)
 {
-	char	buf[100];
-	char	*time;
 	int		i;
 	int		j;
 
 	i = 0;
 	j = 0;
+	while (dst[i] != '\0')
+		i++;
+	while (i < len_dst && src[j] != '\0')
+	{
+		dst[i++] = src[j++];
+	}
+	dst[i] = '\0';
+}
+
+int			print_status(t_phil *phil, char *action)
+{
+	char	buf[LEN_BUF];
+	char	*time;
+
+	buf[0] = '\0';
 	if (phil->state->death)
 		return (0);
 	time = ft_itoa(time_since(phil->state->start_time));
-	while (time[j])
-		buf[i++] = time[j++];
+	ft_strlcat(buf, time, LEN_BUF);
 	free(time);
-	buf[i] = ' ';
-	i++;
-	j = 0;
-	while (phil->id[j])
-		buf[i++] = phil->id[j++];
-	buf[i] = ' ';
-	i++;
-	j = 0;
-	while (action[j])
-		buf[i++] = action[j++];
-	buf[i] = '\n';
-	buf[i + 1] = '\0';
+	ft_strlcat(buf, " ", LEN_BUF);
+	ft_strlcat(buf, phil->id, LEN_BUF);
+	ft_strlcat(buf, " ", LEN_BUF);
+	ft_strlcat(buf, action, LEN_BUF);
+	ft_strlcat(buf, "\n", LEN_BUF);
 	if (phil->state->at_table > 0)
 		write(1, buf, ft_strlen(buf));
 	return (1);
