438e5751e940a16a8938912985089040efb40129
lmalki-h <lmalki-h@student.42.fr>
2021-04-15 10:01:18 +0200

    removed thread for momitoring death and meals to monitor it directly into the parent process

--- philo_three/src/main.c
+++ philo_three/src/main.c
@@ -15,7 +15,6 @@
 int		watch_dog(pid_t *pids, t_state *state)
 {
 	int i = 0;
-	sem_wait(state->finish);
 	while (i < state->nb_phil)
 	{
 		kill(pids[i], SIGTERM);
@@ -24,13 +23,11 @@ int		watch_dog(pid_t *pids, t_state *state)
 	return (SUCCESS);
 }
 
-void	*monitor_routine(void *arg)
+void	*monitor_routine(t_state *state)
 {
 	int		status;
-	t_state *state;
 	int 	finished_eating;
 
-	state = (t_state *)arg;
 	finished_eating = 0;
 	while (finished_eating < state->nb_phil)
 	{
@@ -38,29 +35,13 @@ void	*monitor_routine(void *arg)
 		if (WEXITSTATUS(status) == DEATH)
 		{
 			return ((void *)0);
-
 		}
 		else if (WEXITSTATUS(status) == FINISHED)
 			finished_eating++;
 	}
 	if (finished_eating == state->nb_phil)
 		printf("Each philosopher ate %i times\n", state->nb_meals);
-	sem_post(state->finish);
 	return ((void *)0);
-}
-
-static int monitor_simulation(t_state *state)
-{
-	pthread_t	thread;
-
-	if (state->nb_meals < INT_MAX)
-	{
-		if (pthread_create(&thread, NULL, monitor_routine, (void *)state))
-			return (FAILURE);
-		if (pthread_detach(thread))
-			return (FAILURE);
-	}
-	return (SUCCESS);
 }
 
 static int	start_simulation(t_phil **phils)
@@ -72,11 +53,6 @@ static int	start_simulation(t_phil **phils)
 
 	i = 0;
 	state = phils[0]->state;
-	if (monitor_simulation(state))
-	{
-		write(STDERR_FILENO, "Error: could not start monitoring thread\n", 41);
-		return (FAILURE);
-	}
 	state->start_time = get_time_in_ms();
 	while (i < state->nb_phil)
 	{
@@ -93,7 +69,10 @@ static int	start_simulation(t_phil **phils)
 		exit(SUCCESS);
 	}
 	else
+	{
+		monitor_routine(state);
 		watch_dog(pids, state);
+	}
 	return (SUCCESS);
 }
 
